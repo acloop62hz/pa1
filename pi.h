b3dce1e6030762b6e52c5fe3ea4e671b018d147b
acloop62hz <wanmei326@gmail.com>
2025-01-24 09:52:38 -0500

    first commit

--- pi.h
+++ pi.h
@@ -12,8 +12,24 @@ double pi_calc(long int n) {
     // Write your code below
     ////////////////////////////////////////
     
+    srand(time(NULL));
 
+    long int count_inside_circle = 0;
+
+    // Generate n random points
+    for (long int i = 0; i < n; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+
+        // Check if the point is inside the unit circle
+        if (x * x + y * y <= 1.0) {
+            count_inside_circle++;
+        }
+    }
+
+    // Calculate pi approximation: (points inside circle / total points) * 4
+    double pi_estimate = 4.0 * count_inside_circle / n;
 
     ////////////////////////////////////////
-    return 0.0;
+    return pi_estimate;
 }
