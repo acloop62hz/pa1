abb1559fb74435e9d7bf85163d5421ef39163df1
acloop62hz <wanmei326@gmail.com>
2025-01-24 11:41:44 -0500

    deal with non-perfect divide

--- pi.h
+++ pi.h
@@ -11,13 +11,22 @@ double pi_calc(long int n) {
     
     // Write your code below
     ////////////////////////////////////////
-    
-    srand(time(NULL));
+
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    srand(time(NULL) + rank);
 
     long int count_inside_circle = 0;
+    int np = n/size;
+
+    for(int i = 0; i < n % size; i++){
+        if(rank == i) np = n/size + 1;
+    }
 
     // Generate n random points
-    for (long int i = 0; i < n; i++) {
+    for (long int i = 0; i < np; i++) {
         double x = (double)rand() / RAND_MAX;
         double y = (double)rand() / RAND_MAX;
 
@@ -27,8 +36,10 @@ double pi_calc(long int n) {
         }
     }
 
-    // Calculate pi approximation: (points inside circle / total points) * 4
-    double pi_estimate = 4.0 * count_inside_circle / n;
+    long int global_sum = 0;
+    MPI_Reduce(&count_inside_circle, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    double pi_estimate = 4.0 * (double)global_sum / (double)n;
 
     ////////////////////////////////////////
     return pi_estimate;
