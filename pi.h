188a257da53e3143e29adbe76590bf3b43cee58f
Xinyi Zhou <xzhou487@login-ice-2.pace.gatech.edu>
2025-01-24 11:19:11 -0500

    correctness, scalability passed, runtime failed

--- pi.h
+++ pi.h
@@ -12,8 +12,34 @@ double pi_calc(long int n) {
     // Write your code below
     ////////////////////////////////////////
     
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    srand(time(NULL) + rank);
+
+    long int count_inside_circle = 0;
+
+    // Generate n random points
+    for (long int i = 0; i < n/size; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+
+        // Check if the point is inside the unit circle
+        if (x * x + y * y <= 1.0) {
+            count_inside_circle++;
+        }
+    }
+
+    long int global_sum = 0;
+    MPI_Reduce(&count_inside_circle, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // if (rank == 0) {
+    //     printf("%ld\n", global_sum);
+    // }
+    // Calculate pi approximation: (points inside circle / total points) * 4
+    double pi_estimate = 4.0 * (double)global_sum / (double)n;
 
 
     ////////////////////////////////////////
-    return 0.0;
+    return pi_estimate;
 }
