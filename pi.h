be177a655442f37b3f096477af7afd908be87ae6
Alex Jiang <alexjiang04@gmail.com>
2025-01-24 11:09:44 -0500

    Alex

--- pi.h
+++ pi.h
@@ -6,14 +6,43 @@
 #include <iostream>
 #include <mpi.h>
 
+void helper(const void *in, void *out, int *len, MPI_Datatype *datatype){
+    const long int *in = (const long int *)in;
+    double *inout = (double *) out;
+
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    srand(time(NULL)+rank);
+
+    for(int i; i<*len; i++){
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+        if (x * x + y * y <= 1.0) {
+            *out++;
+        }
+    }
+}
 
 double pi_calc(long int n) {
-    
-    // Write your code below
-    ////////////////////////////////////////
-    
+
+    long int count_inside_circle = 0;
+
+    // Generate n random points
+    MPI_Op count;
+    MPI_Op_create(helper, 1, &count);
+
+    MPI_Reduce(&n, &count_inside_circle, 1, MPI_DOUBLE, count, 0, MPI_COMM_WORLD);
+
+    // Calculate pi approximation: (points inside circle / total points) * 4
+    double pi_estimate = 4.0 * count_inside_circle / n;
 
 
     ////////////////////////////////////////
-    return 0.0;
+    return pi_estimate;
 }
+
+
+
+
